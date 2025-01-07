6894b8792879fe7ce4906e0c53eba8b9680361d0
Evan <evan@evanalexander.dev>
2025-01-07 15:36:29 -0700

    W01 Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,20 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Create a new array to hold the multiples
+        double[] multiples = new double[length];
 
-        return []; // replace this return statement with your own
+        // Initialize the first element with the starting number
+        multiples[0] = number;
+
+        // Fill the rest of the array with the other multiples, using a for loop
+        for (int i = 1; i < length; i++) {
+            // Each multiple is the previous multiple plus the starting number
+            multiples[i] = multiples[i - 1] + number;
+        }
+
+        // Return the array of multiples
+        return multiples;
     }
 
     /// <summary>
@@ -23,11 +31,18 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
-    public static void RotateListRight(List<int> data, int amount)
-    {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+    public static void RotateListRight(List<int> data, int amount) {
+        // Divide the list into 2 parts
+        // Get the index of where to split
+        var splitIndex = data.Count - amount;
+        // Get the part to rotate (the back end of the list)
+        var rotated = data.Skip(splitIndex).ToList();
+        // Get whatever is the remaining part (whatever we didn't take off the front end of the list)
+        var left = data.Take(splitIndex).ToList();
+
+        // Rebuild the list, in rotated order
+        data.Clear();
+        data.AddRange(rotated);
+        data.AddRange(left);
     }
 }
